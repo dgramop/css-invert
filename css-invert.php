64fc12697047e072aae8f414f0624836f5e3c84c
Dhruv Gramopadhye <dgramopadhye@gmail.com>
2017-03-29 18:14:04 -0400

    You forgot a return statement
    I fixed it!

--- css-invert.php
+++ css-invert.php
@@ -1,27 +1,31 @@
 #!/usr/bin/php
 
 <?php
-
+if(!(in_array("help", $argv) || in_array("-h", $argv)) && $argc>=3)
+{
 //$file=fopen("./site.css","r");
-$file="./invert-me.css.css";
-
-
+$filein=$argv[$argc-2];
+$fileout=$argv[$argc-1];
+//The whole argc-n is a little hack to make this work using both "php css-invert.php filein.css fileout.css" and "./css-invert.php filein.css fileout.css"
 function inverseColors($css) {
     preg_match_all('/#([a-f0-9]{6}|[a-f0-9]{3})/i', $css, $matches);
     $original = $matches[0];
     $inversed = array();
-    foreach($matches[1] as $key => $color) {    
+    foreach($matches[1] as $key => $color) {
         $parts = str_split($color, strlen($color) == 3 ? 1 : 2);
         foreach($parts as &$part) {
             $part = str_pad(dechex(255 - hexdec($part)), 2, 0, STR_PAD_LEFT);
         }
-        $inversed[$key] = '#'.implode('', $parts);    
+        $inversed[$key] = '#'.implode('', $parts);
     }
-
     $css = str_replace($original, $inversed, $css);
-    echo $css;
+    return $css;
 }
-
-file_put_contents($file, inverseColors(file_get_contents($file)));
+file_put_contents($fileout, inverseColors(file_get_contents($filein)));
+}
+else
+{
+    echo "USAGE:\n php css-invert.php <filein.php> <fileout.php> [-h] [--help] OR ./css-invert.php <filein.php> <fileout.php> [-h] [--help]\n\n Flags -h, --help for this help dialog";
+}
 
 ?>
